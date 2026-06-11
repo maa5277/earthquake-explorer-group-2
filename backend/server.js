e47c3f75ba1de569b02dc46ce5ffeb0e2a190238
Max Murray <mjm4243@g.rit.edu>
2026-06-11 13:56:18 -0400

    Added mongodb backend and api routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,48 @@
 const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Earthquake Explorer Running");
-});
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
-});
+const client = new MongoClient(process.env.MONGO_URI);
+
+let db;
+
+async function startServer() {
+    await client.connect();
+
+    db = client.db(process.env.DB_NAME);
+
+    app.get("/", (req, res) => {
+        res.json({
+            message: "Earthquake API running"
+        });
+    });
+
+    app.get("/api/count", async (req, res) => {
+        const count = await db.collection("earthquakes").countDocuments();
+        res.json({ count });
+    });
+
+    app.get("/api/earthquakes", async (req, res) => {
+        const docs = await db
+            .collection("earthquakes")
+            .find({})
+            .limit(20)
+            .toArray();
+
+        res.json(docs);
+    });
+
+    const PORT = process.env.PORT || 3001;
+
+    app.listen(PORT, () => {
+	console.log('Server running on port ${PORT}');
+    });
+}
+
+startServer().catch(console.error);
